80345353f69e6a68183b878570fefa14179959b7
Yukuiii <472619563@qq.com>
2025-11-28 17:25:22 +0800

    refactor(source): 重构站点配置到独立 sites 目录
    - 新增 sites/ 目录存放各网站解析逻辑
    - 将笔趣阁配置移至 sites/biquge.ts
    - builtinSources.ts 改为从 sites 模块导入

--- src/webview/src/core/source/builtinSources.ts
+++ src/webview/src/core/source/builtinSources.ts
@@ -6,33 +6,14 @@
 import type { EsoSource } from "./esoTypes";
 import { EsoContentType } from "./esoTypes";
 
+// 导入各站点书源配置
+import { biqugeSource } from "./sites";
+
 /**
  * 内置书源列表
  */
 export const builtinSources: EsoSource[] = [
-  {
-    id: "builtin-biquge-xin",
-    name: "笔趣阁xin",
-    host: "https://www.biquge.xin",
-    contentType: EsoContentType.Novel,
-    author: "内置书源",
-    // 搜索配置
-    enableSearch: true,
-    searchUrl: "/search/result.html?searchtype=novelname&searchkey={{keyword}}",
-    searchList: "ul.librarylist li",
-    searchName: "span@text",
-    searchAuthor: "span:nth-of-type(2) a@text",
-    searchCover: "img@src",
-    searchDescription: ".intro@text",
-    searchChapter: "p:last-of-type@text##最新章节[:：]",
-    searchResult: "a@href",
-    // 章节列表配置
-    chapterList: "ul.three li a",
-    chapterName: "@text##【正.*广】",
-    chapterResult: "@href",
-    // 正文配置
-    contentItems: "#chaptercontent p@html",
-  },
+  biqugeSource,
 ];
 
 /**
---
+++ src/webview/src/core/source/sites/index.ts
@@ -0,0 +1,7 @@
+/**
+ * 站点模块导出
+ * 各小说网站的解析逻辑和配置
+ */
+
+// 笔趣阁
+export { biqugeSource } from "./biquge";
